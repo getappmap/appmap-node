fb070dc75caa2c875c28a0c5a782853a2fdbd192
Rafał Rzepecki <divided.mind@gmail.com>
2023-08-18 13:59:57 +0200

    New initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/* eslint-env node */
+
+/** @type {import('@typescript-eslint/utils/dist/ts-eslint').Linter.Config} */
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
+    "prettier",
+  ],
+
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+      ],
+      files: ["./**/*.{ts,tsx}"],
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+};
