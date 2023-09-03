0a7ef0acbbae75ef0b33726bab5f14253451d841
Rafał Rzepecki <divided.mind@gmail.com>
2023-09-02 20:59:18 -0400

    Allow writing out transformed source for debugging

--- src/transform.ts
+++ src/transform.ts
@@ -2,10 +2,15 @@ import { generate } from "astring";
 import { parse, type ESTree } from "meriyah";
 import assert from "node:assert";
 import { warn } from "node:console";
+import { fileURLToPath } from "node:url";
+import { debuglog } from "node:util";
 import { isNativeError } from "node:util/types";
 
 import * as instrument from "./hooks/instrument";
 import * as jest from "./hooks/jest";
+import { writeFileSync } from "node:fs";
+
+const treeDebug = debuglog("appmap-tree");
 
 export interface Hook {
   shouldInstrument(url: URL): boolean;
@@ -21,10 +26,18 @@ export default function transform(code: string, url: URL): string {
   try {
     const tree = parse(code, { source: url.toString(), next: true, loc: true });
     const xformed = hook.transform(tree);
+    if (treeDebug.enabled) dumpTree(xformed, url);
     return generate(xformed);
   } catch (e) {
     assert(isNativeError(e));
     warn("Error transforming source at %s: %s", url, e.message);
     return code;
   }
+}
+
+function dumpTree(xformed: ESTree.Program, url: URL) {
+  if (url.protocol !== "file:") return;
+  const path = fileURLToPath(url) + ".appmap-tree.json";
+  writeFileSync(path, JSON.stringify(xformed, null, 2));
+  treeDebug("wrote transformed tree to %s", path);
 }
