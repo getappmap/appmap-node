aa58977015f295be62e36859cb00c7700b867de1
Rafał Rzepecki <divided.mind@gmail.com>
2023-09-10 19:40:48 -0400

    Add a runner script

---
+++ bin/appmap-node.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+require("../dist/bin.js").default();
---
+++ src/bin.ts
@@ -0,0 +1,42 @@
+import { spawn } from "node:child_process";
+import { accessSync } from "node:fs";
+import { resolve } from "node:path";
+
+const registerPath = resolve(__dirname, "../dist/register.js");
+
+export default function main() {
+  addNodeOptions("--require", registerPath);
+  const [cmd, ...args] = process.argv.slice(2);
+
+  if (isScript(cmd)) {
+    process.argv.splice(1, 1); // remove outselves from argv
+    runScript(cmd);
+  } else {
+    // it's a command, spawn it
+    spawn(cmd, args, { stdio: "inherit" });
+  }
+}
+
+function addNodeOptions(...options: string[]) {
+  const envar = (process.env.NODE_OPTIONS ?? "").split(" ");
+  envar.push(...options);
+  process.env.NODE_OPTIONS = envar.join(" ");
+}
+
+/* Heuristic to check if argument is a node script. Currently just checks if it's an existing file
+with a known extension; the assumption being if it's not, then it's probably a command. */
+function isScript(arg: string) {
+  try {
+    accessSync(arg);
+    return arg.match(/.*\.[mc]?[tj]s/);
+  } catch {
+    return false;
+  }
+}
+
+function runScript(path: string) {
+  require(registerPath);
+  require(resolve(path));
+}
+
+if (require.main === module) main();
