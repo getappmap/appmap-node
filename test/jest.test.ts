ba715447ac41dc59a47c640472cb602e7306ab03
Rafał Rzepecki <divided.mind@gmail.com>
2023-10-15 13:46:30 -0400

    feat: Pass the child return code when spawning

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -1,6 +1,6 @@
 import { integrationTest, readAppmaps, runAppmapNode } from "./helpers";
 
 integrationTest("mapping Jest tests", () => {
-  runAppmapNode("yarn", "jest");
+  expect(runAppmapNode("yarn", "jest").status).toBe(1);
   expect(readAppmaps()).toMatchSnapshot();
 });
