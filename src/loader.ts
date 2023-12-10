172cddd30c15d6c29988b844fb74e84402b175d6
zermelo-wisen <zermelo@wisen.tech>
2023-12-10 21:40:23 +0300

    fix: Handle extension-less files in Node 18
    Fix our custom loader to handle extension-less files in Node 18 as CommonJS when required.

--- src/loader.ts
+++ src/loader.ts
@@ -1,10 +1,29 @@
+import path from "node:path";
+import { URL } from "node:url";
+
 import type { NodeLoaderHooksAPI2 } from "ts-node";
 
+import { targetPackage } from "./config";
 import { warn } from "./message";
 import transform, { findHook } from "./transform";
 
 export const load: NodeLoaderHooksAPI2["load"] = async function load(url, context, defaultLoad) {
-  const hook = findHook(new URL(url));
+  const urlObj = new URL(url);
+
+  // With a custom loader, in node 18, extensionless files (typically used
+  // as main entry points inside bin folders) are trated as ESM rather than
+  // CommonJS modules. This leads to TypeError [ERR_UNKNOWN_FILE_EXTENSION].
+  // To fix this, we give the context.format hint to defaultLoad if we decide
+  // that the file is a CommonJS module.
+  const [major] = process.versions.node.split(".").map(Number);
+  if (major == 18) {
+    const ext = path.extname(urlObj.pathname).slice(1);
+    if (ext == "" && urlObj.protocol == "file:" && targetPackage?.type != "module") {
+      context.format = "commonjs";
+    }
+  }
+
+  const hook = findHook(urlObj);
   if (hook) {
     const original = await defaultLoad(url, context, defaultLoad);
 
