c94a328fb2dcf2a25dda1b3cb845873c8a45b889
zermelo-wisen <zermelo@wisen.tech>
2024-01-05 11:32:17 +0300

    feat: Http capture support in ESM

--- test/httpClient.test.ts
+++ test/httpClient.test.ts
@@ -3,7 +3,7 @@ import http from "node:http";
 import { integrationTest, readAppmap, runAppmapNode, spawnAppmapNode } from "./helpers";
 import { SERVER_PORT, TEST_HEADER_VALUE } from "./httpClient";
 
-integrationTest("mapping http client requests", async () => {
+const httpClientRequestsTest = async (script: string) => {
   const server = http
     .createServer(function (req, res) {
       if (req.url?.startsWith("/endpoint/one") ?? req.url?.startsWith("endpoint/two")) {
@@ -15,15 +15,20 @@ integrationTest("mapping http client requests", async () => {
       }
     })
     .listen(SERVER_PORT);
-  const client = spawnAppmapNode("yarn", "exec", "ts-node", "index.ts");
+  const client = spawnAppmapNode("yarn", "exec", "ts-node", script);
   await new Promise<void>((r) => client.on("close", () => r()));
   server.close();
   const appMap = readAppmap();
 
+  expect(appMap).toMatchSnapshot();
+
   // Make sure we capture the headers modified/added after ClientRequest creation.
   expect(JSON.stringify(appMap.events)).toContain(TEST_HEADER_VALUE);
-  expect(appMap).toMatchSnapshot();
-});
+};
+
+integrationTest("mapping http client requests (ESM)", () => httpClientRequestsTest("esm.mts"));
+
+integrationTest("mapping http client requests", () => httpClientRequestsTest("index.ts"));
 
 integrationTest("mapping mocked http client requests", () => {
   expect(runAppmapNode("yarn", "exec", "ts-node", "index.ts", "--mock").status).toBe(0);
