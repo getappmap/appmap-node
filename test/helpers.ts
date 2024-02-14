19ac8f4e2a487838e6b37f7133f0ef54d95461dd
Rafał Rzepecki <divided.mind@gmail.com>
2024-02-14 17:39:30 -0500

    test: Remove symbols from snapshots which might differ across versions

--- test/helpers.ts
+++ test/helpers.ts
@@ -122,9 +122,10 @@ function fixHttp(http: unknown) {
 function fixValue(value: unknown): void {
   if (value && typeof value === "object" && "value" in value && typeof value.value === "string") {
     const v = value.value;
-    if (v.startsWith("Next")) value.value = value.value.split(" ")[0];
+    if (v.startsWith("Next")) value.value = v.split(" ")[0];
     else if (v.includes("ObjectId"))
       value.value = v.replaceAll(/ObjectId\('.*'\)/g, "ObjectId('test')");
+    else value.value = v.replaceAll(/\s+\[Symbol.*/g, "");
   }
 }
 
