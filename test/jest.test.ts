44e0c497ea2495d00c85d32639d8e865b9f0167b
zermelo-wisen <zermelo@wisen.tech>
2024-03-22 00:33:36 +0300

    fix: Instrumented async lambda wrapper should be async

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -1,6 +1,7 @@
 import { integrationTest, readAppmaps, runAppmapNode } from "./helpers";
 
 integrationTest("mapping Jest tests", () => {
-  expect(runAppmapNode("yarn", "jest", "--color").status).toBe(1);
+  expect(runAppmapNode("yarn", "jest", "test", "--color").status).toBe(1);
+  expect(runAppmapNode("yarn", "jest", "asyncLib", "--color").status).toBe(0);
   expect(readAppmaps()).toMatchSnapshot();
 });
