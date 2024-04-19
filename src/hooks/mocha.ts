1fa7b88780a1c837b6caa7e236eeb6df36b4ab6f
zermelo-wisen <zermelo@wisen.tech>
2024-04-19 13:17:35 +0300

    fix: Report test source location (file only) - Mocha

--- src/hooks/mocha.ts
+++ src/hooks/mocha.ts
@@ -1,18 +1,20 @@
 import assert from "node:assert";
+import { relative } from "node:path";
 import { EventEmitter } from "node:stream";
 
 import { simple as walk } from "acorn-walk";
 import type { ESTree } from "meriyah";
 
 import { expressionFor } from ".";
+import config from "../config";
 import { call_, this_ } from "../generate";
+import { info } from "../message";
+import { exceptionMetadata } from "../metadata";
 import {
   abandonProcessRecordingIfNotAlwaysActive,
   getTestRecording,
   startTestRecording,
 } from "../recorder";
-import { info } from "../message";
-import { exceptionMetadata } from "../metadata";
 
 export function shouldInstrument(url: URL): boolean {
   return url.pathname.endsWith("/mocha/lib/runner.js");
@@ -49,6 +51,7 @@ export function transform(program: ESTree.Program): ESTree.Program {
    Instead, define the minimal needed type surface here. */
 interface Test {
   titlePath: () => string[];
+  file: string;
 }
 
 const EVENT_TEST_BEGIN = "test";
@@ -57,7 +60,8 @@ const EVENT_TEST_FAIL = "fail";
 
 function registerEventListeners(runner: EventEmitter) {
   runner.on(EVENT_TEST_BEGIN, function (test: Test) {
-    startTestRecording("mocha", ...test.titlePath());
+    const recording = startTestRecording("mocha", ...test.titlePath());
+    if (test.file) recording.metadata.source_location = relative(config.root, test.file);
   });
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -67,11 +71,14 @@ function registerEventListeners(runner: EventEmitter) {
     recording.finish();
   });
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   runner.on(EVENT_TEST_FAIL, function (test: Test, err: unknown) {
     const recording = getTestRecording();
     recording.metadata.test_status = "failed";
     recording.metadata.exception = exceptionMetadata(err);
+    recording.metadata.test_failure = {
+      message: recording.metadata.exception?.message ?? "failed",
+      location: test.file ? relative(config.root, test.file) : undefined,
+    };
     recording.finish();
   });
 }
